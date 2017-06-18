af77ff6cacdbbfa0b5483345fa1878f881722374
Brian Porter <brianwp3000@gmail.com>
2017-06-18 14:50:16 -0700

    added unary operators

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@
 #[macro_export]
 macro_rules! impl_op {
     ($op:tt |$lhs_i:ident : &mut $lhs:path, $rhs_i:ident : &$rhs:path| $body:block) => (_parse_assignment_op!($op, $lhs, $rhs, |$lhs_i : &mut $lhs, $rhs_i : &$rhs| $body););
+    ($op:tt |$lhs_i:ident : &$lhs:path|  -> $out:path $body:block) => (_parse_unary_op!($op, $lhs, $out, |$lhs_i : &$lhs| -> $out {$body}););
     ($op:tt |$lhs_i:ident : &$lhs:path, $rhs_i:ident : &$rhs:path| -> $out:path $body:block) => (_parse_binary_op!($op, $lhs, $rhs, $out, |$lhs_i : &$lhs, $rhs_i : &$rhs| -> $out {$body}););
 }
 
@@ -40,6 +41,12 @@ macro_rules! _parse_assignment_op {
     (^=, $($t:tt)+) => (_impl_assignment_op_internal!(BitXorAssign, bitxor_assign, $($t)+););
     (<<=, $($t:tt)+) => (_impl_assignment_op_internal!(ShlAssign, shl_assign, $($t)+););
     (>>=, $($t:tt)+) => (_impl_assignment_op_internal!(ShrAssign, shr_assign, $($t)+););
+}
+
+#[macro_export]
+macro_rules! _parse_unary_op {
+    (-, $($t:tt)+) => (_impl_unary_op_internal!(Neg, neg, $($t)+););
+    (!, $($t:tt)+) => (_impl_unary_op_internal!(Not, not, $($t)+););
 }
 
 #[macro_export]
@@ -129,5 +136,26 @@ macro_rules! _impl_assignment_op_internal {
             }
         }
         
+    );
+}
+
+#[macro_export]
+macro_rules! _impl_unary_op_internal {
+    ($ops_trait:ident, $ops_fn:ident, $lhs:ty, $out:ty, $fn:expr) => (        
+        impl ops::$ops_trait for $lhs {
+            type Output = $out;
+
+            fn $ops_fn(self) -> Self::Output {
+                $fn(&self)
+            }
+        }
+
+        impl<'a> ops::$ops_trait for &'a $lhs {
+            type Output = $out;
+
+            fn $ops_fn(self) -> Self::Output {
+                $fn(self)
+            }
+        }
     );
 }
---
+++ tests/unary.rs
@@ -0,0 +1,55 @@
+// #![feature(trace_macros)]
+// trace_macros!(true);
+
+#[macro_use]
+extern crate impl_ops;
+
+use std::ops;
+
+mod kong {
+    #[derive(Clone, Copy, Debug, Default, PartialEq)]
+    pub struct Donkey {
+        pub bananas: i32,
+    }
+
+    impl Donkey {
+        pub fn new(bananas: i32) -> Donkey {
+            Donkey { bananas }
+        }
+    }
+
+    #[derive(Clone, Copy, Debug, Default, PartialEq)]
+    pub struct Diddy<T> {
+        pub bananas: T,
+    }
+
+    impl Diddy<i32> {
+        pub fn new(bananas: i32) -> Diddy<i32> {
+            Diddy { bananas }
+        }
+    }
+}
+
+impl_op!(- |a: &kong::Donkey| -> kong::Diddy<i32> {
+    let lhs = a;
+    kong::Diddy::<i32>::new(lhs.bananas)
+});
+impl_op!(! |a: &kong::Donkey| -> kong::Diddy<i32> { kong::Diddy::<i32>::new(a.bananas)});
+
+macro_rules! impl_op_test {
+    ($test:ident, $op:tt($lhs:expr) -> ($expected:expr)) => (
+        #[test]
+        fn $test() {
+            let lhs = $lhs;
+            let expected = $expected;
+
+            let actual = $op lhs;
+            assert_eq!(expected, actual, "<op> owned");
+            let actual = $op &lhs;
+            assert_eq!(expected, actual, "<op> borrowed");
+        }
+    );
+}
+
+impl_op_test!(neg, -(kong::Donkey::new(2)) -> (kong::Diddy::<i32>::new(2)));
+impl_op_test!(not, !(kong::Donkey::new(2)) -> (kong::Diddy::<i32>::new(2)));
