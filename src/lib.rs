9f251f700fbeeaecf37ae3d5015d0f567c7ac3bd
Brian Porter <brianwp3000@gmail.com>
2017-06-27 21:14:55 -0700

    impl_ops_ex (#8)
    * separated impl_op and impl_op_ex, finished Binary and rewrote tests
    
    * rewrote assignment ops
    
    * rewrote unary ops
    
    * renamed binary tests to match other tests
    
    * multiline tests
    
    * spacing nit

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,115 @@
 //#![feature(trace_macros)]
-//#![feature(log_syntax)]
-
 //trace_macros!(true);
 
 #[macro_export]
 macro_rules! impl_op {
-    ($op:tt |$lhs_i:ident : &mut $lhs:path, $rhs_i:ident : &$rhs:path| $body:block) => (_parse_assignment_op!($op, $lhs, &$rhs, |$lhs_i : &mut $lhs, $rhs_i : &$rhs| $body););
-    ($op:tt |$lhs_i:ident : &mut $lhs:path, $rhs_i:ident : $rhs:path| $body:block) => (_parse_assignment_op!($op, $lhs, $rhs, |$lhs_i : &mut $lhs, $rhs_i : $rhs| $body););
-    ($op:tt |$lhs_i:ident : &$lhs:path|  -> $out:path $body:block) => (_parse_unary_op!($op, &$lhs, $out, |$lhs_i : &$lhs| -> $out {$body}););
-    ($op:tt |$lhs_i:ident : &$lhs:path, $rhs_i:ident : &$rhs:path| -> $out:path $body:block) => (_parse_binary_op!($op, &$lhs, &$rhs, $out, |$lhs_i : &$lhs, $rhs_i : &$rhs| -> $out {$body}););
-    ($op:tt |$lhs_i:ident : &$lhs:path, $rhs_i:ident : $rhs:path| -> $out:path $body:block) => (_parse_binary_op!($op, &$lhs, $rhs, $out, |$lhs_i : &$lhs, $rhs_i : $rhs| -> $out {$body}););
-    ($op:tt |$lhs_i:ident : $lhs:path|  -> $out:path $body:block) => (_parse_unary_op!($op, $lhs, $out, |$lhs_i : $lhs| -> $out {$body}););
-    ($op:tt |$lhs_i:ident : $lhs:path, $rhs_i:ident : &$rhs:path| -> $out:path $body:block) => (_parse_binary_op!($op, $lhs, &$rhs, $out, |$lhs_i : $lhs, $rhs_i : &$rhs| -> $out {$body}););
-    ($op:tt |$lhs_i:ident : $lhs:path, $rhs_i:ident : $rhs:path| -> $out:path $body:block) => (_parse_binary_op!($op, $lhs, $rhs, $out, |$lhs_i : $lhs, $rhs_i : $rhs| -> $out {$body}););
+    ($op:tt |$lhs_i:ident : &mut $lhs:path, $rhs_i:ident : &$rhs:path| $body:block) => (
+        _parse_assignment_op!($op, $lhs, &$rhs, lhs, rhs, {|$lhs_i : &mut $lhs, $rhs_i : &$rhs| -> () {$body} (lhs, rhs);});
+    );
+    ($op:tt |$lhs_i:ident : &mut $lhs:path, $rhs_i:ident : $rhs:path| $body:block) => (
+        _parse_assignment_op!($op, $lhs, $rhs, lhs, rhs, {|$lhs_i : &mut $lhs, $rhs_i : $rhs| -> () {$body} (lhs, rhs);});
+    );
+    ($op:tt |$lhs_i:ident : &$lhs:path| -> $out:path $body:block) => (
+        _parse_unary_op!($op, &$lhs, $out, lhs, {|$lhs_i : &$lhs| -> $out {$body} (lhs)});
+    );
+    ($op:tt |$lhs_i:ident : &$lhs:path, $rhs_i:ident : &$rhs:path| -> $out:path $body:block) => (
+        _parse_binary_op!($op, &$lhs, &$rhs, $out, lhs, rhs, {|$lhs_i : &$lhs, $rhs_i : &$rhs| -> $out {$body} (lhs, rhs)});
+    );
+    ($op:tt |$lhs_i:ident : &$lhs:path, $rhs_i:ident : $rhs:path| -> $out:path $body:block) => (
+        _parse_binary_op!($op, &$lhs, $rhs, $out, lhs, rhs, {|$lhs_i : &$lhs, $rhs_i : $rhs| -> $out {$body} (lhs, rhs)});
+    );
+    ($op:tt |$lhs_i:ident : $lhs:path| -> $out:path $body:block) => (
+        _parse_unary_op!($op, $lhs, $out, lhs, {|$lhs_i : $lhs| -> $out {$body} (lhs)});
+    );
+    ($op:tt |$lhs_i:ident : $lhs:path, $rhs_i:ident : &$rhs:path| -> $out:path $body:block) => (
+        _parse_binary_op!($op, $lhs, &$rhs, $out, lhs, rhs, {|$lhs_i : $lhs, $rhs_i : &$rhs| -> $out {$body} (lhs, rhs)});
+    );
+    ($op:tt |$lhs_i:ident : $lhs:path, $rhs_i:ident : $rhs:path| -> $out:path $body:block) => (
+        _parse_binary_op!($op, $lhs, $rhs, $out, lhs, rhs, {|$lhs_i : $lhs, $rhs_i : $rhs| -> $out {$body} (lhs, rhs)});
+    );
+}
+
+#[macro_export]
+macro_rules! impl_op_ex {
+    ($op:tt |$lhs_i:ident : &mut $lhs:path, $rhs_i:ident : &$rhs:path| $body:block) => (
+        _parse_assignment_op!($op, $lhs, &$rhs, lhs, rhs, {|$lhs_i : &mut $lhs, $rhs_i : &$rhs| -> () {$body} (lhs, rhs);});
+        _parse_assignment_op!($op, $lhs, $rhs, lhs, rhs, {|$lhs_i : &mut $lhs, $rhs_i : &$rhs| -> () {$body} (lhs, &rhs);});
+    );
+    ($op:tt |$lhs_i:ident : &mut $lhs:path, $rhs_i:ident : $rhs:path| $body:block) => (
+        _parse_assignment_op!($op, $lhs, $rhs, lhs, rhs, {|$lhs_i : &mut $lhs, $rhs_i : $rhs| -> () {$body} (lhs, rhs);});
+    );
+    ($op:tt |$lhs_i:ident : &$lhs:path| -> $out:path $body:block) => (
+        _parse_unary_op!($op, &$lhs, $out, lhs, {|$lhs_i : &$lhs| -> $out {$body} (lhs)});
+        _parse_unary_op!($op, $lhs, $out, lhs, {|$lhs_i : &$lhs| -> $out {$body} (&lhs)});
+    );
+    ($op:tt |$lhs_i:ident : &$lhs:path, $rhs_i:ident : &$rhs:path| -> $out:path $body:block) => (
+        impl_op!($op |$lhs_i : &$lhs, $rhs_i : &$rhs| -> $out $body);
+        _parse_binary_op!($op, &$lhs, $rhs, $out, lhs, rhs, {|$lhs_i : &$lhs, $rhs_i : &$rhs| -> $out {$body} (lhs, &rhs)});
+        _parse_binary_op!($op, $lhs, &$rhs, $out, lhs, rhs, {|$lhs_i : &$lhs, $rhs_i : &$rhs| -> $out {$body} (&lhs, rhs)});
+        _parse_binary_op!($op, $lhs, $rhs, $out, lhs, rhs, {|$lhs_i : &$lhs, $rhs_i : &$rhs| -> $out {$body} (&lhs, &rhs)});
+    );
+    ($op:tt |$lhs_i:ident : &$lhs:path, $rhs_i:ident : $rhs:path| -> $out:path $body:block) => (
+        impl_op!($op |$lhs_i : &$lhs, $rhs_i : $rhs| -> $out $body);
+        _parse_binary_op!($op, $lhs, $rhs, $out, lhs, rhs, {|$lhs_i : &$lhs, $rhs_i : $rhs| -> $out {$body} (&lhs, rhs)});
+    );
+    ($op:tt |$lhs_i:ident : $lhs:path|  -> $out:path $body:block) => (
+        _parse_unary_op!($op, $lhs, $out, lhs, {|$lhs_i : $lhs| -> $out {$body} (lhs)});
+    );
+    ($op:tt |$lhs_i:ident : $lhs:path, $rhs_i:ident : &$rhs:path| -> $out:path $body:block) => (
+        impl_op!($op |$lhs_i : $lhs, $rhs_i : &$rhs| -> $out $body);
+        _parse_binary_op!($op, $lhs, $rhs, $out, lhs, rhs, {|$lhs_i : $lhs, $rhs_i : &$rhs| -> $out {$body} (lhs, &rhs)});
+    );
+    ($op:tt |$lhs_i:ident : $lhs:path, $rhs_i:ident : $rhs:path| -> $out:path $body:block) => (
+        impl_op!($op |$lhs_i : $lhs, $rhs_i : $rhs| -> $out $body);
+    );
 }
 
 #[macro_export]
 macro_rules! impl_op_commutative {
-    ($op:tt |$lhs_i:ident : &$lhs:path, $rhs_i:ident : &$rhs:path| -> $out:path $body:block) => (_parse_binary_op!($op, [commutative], &$lhs, &$rhs, $out, |$lhs_i : &$lhs, $rhs_i : &$rhs| -> $out {$body}););
-    ($op:tt |$lhs_i:ident : &$lhs:path, $rhs_i:ident : $rhs:path| -> $out:path $body:block) => (_parse_binary_op!($op, [commutative], &$lhs, $rhs, $out, |$lhs_i : &$lhs, $rhs_i : $rhs| -> $out {$body}););
-    ($op:tt |$lhs_i:ident : $lhs:path, $rhs_i:ident : &$rhs:path| -> $out:path $body:block) => (_parse_binary_op!($op, [commutative], $lhs, &$rhs, $out, |$lhs_i : $lhs, $rhs_i : &$rhs| -> $out {$body}););
-    ($op:tt |$lhs_i:ident : $lhs:path, $rhs_i:ident : $rhs:path| -> $out:path $body:block) => (_parse_binary_op!($op, [commutative], $lhs, $rhs, $out, |$lhs_i : $lhs, $rhs_i : $rhs| -> $out {$body}););}
+    ($op:tt |$lhs_i:ident : &$lhs:path, $rhs_i:ident : &$rhs:path| -> $out:path $body:block) => (
+        impl_op!($op |$lhs_i : &$lhs, $rhs_i : &$rhs| -> $out $body);
+        _parse_binary_op!($op, &$rhs, &$lhs, $out, lhs, rhs, {|$lhs_i : &$lhs, $rhs_i : &$rhs| -> $out {$body} (rhs, lhs)});
+    );
+    ($op:tt |$lhs_i:ident : &$lhs:path, $rhs_i:ident : $rhs:path| -> $out:path $body:block) => (
+        impl_op!($op |$lhs_i : &$lhs, $rhs_i : $rhs| -> $out $body);
+        _parse_binary_op!($op, $rhs, &$lhs, $out, lhs, rhs, {|$lhs_i : &$lhs, $rhs_i : $rhs| -> $out {$body} (rhs, lhs)});
+    );
+    ($op:tt |$lhs_i:ident : $lhs:path, $rhs_i:ident : &$rhs:path| -> $out:path $body:block) => (
+        impl_op!($op |$lhs_i : $lhs, $rhs_i : &$rhs| -> $out $body);
+        _parse_binary_op!($op, &$rhs, $lhs, $out, lhs, rhs, {|$lhs_i : $lhs, $rhs_i : &$rhs| -> $out {$body} (rhs, lhs)});
+    );
+    ($op:tt |$lhs_i:ident : $lhs:path, $rhs_i:ident : $rhs:path| -> $out:path $body:block) => (
+        impl_op!($op |$lhs_i : $lhs, $rhs_i : $rhs| -> $out $body);
+        _parse_binary_op!($op, $rhs, $lhs, $out, lhs, rhs, {|$lhs_i : $lhs, $rhs_i : $rhs| -> $out {$body} (rhs, lhs)});
+    );
+}
+
+#[macro_export]
+macro_rules! impl_op_ex_commutative {
+    ($op:tt |$lhs_i:ident : &$lhs:path, $rhs_i:ident : &$rhs:path| -> $out:path $body:block) => (
+        impl_op_ex!($op |$lhs_i : &$lhs, $rhs_i : &$rhs| -> $out $body);
+
+        _parse_binary_op!($op, &$rhs, &$lhs, $out, lhs, rhs, {|$lhs_i : &$lhs, $rhs_i : &$rhs| -> $out {$body} (rhs, lhs)});
+        _parse_binary_op!($op, &$rhs, $lhs, $out, lhs, rhs, {|$lhs_i : &$lhs, $rhs_i : &$rhs| -> $out {$body} (&rhs, lhs)});
+        _parse_binary_op!($op, $rhs, &$lhs, $out, lhs, rhs, {|$lhs_i : &$lhs, $rhs_i : &$rhs| -> $out {$body} (rhs, &lhs)});
+        _parse_binary_op!($op, $rhs, $lhs, $out, lhs, rhs, {|$lhs_i : &$lhs, $rhs_i : &$rhs| -> $out {$body} (&rhs, &lhs)});
+    );
+    ($op:tt |$lhs_i:ident : &$lhs:path, $rhs_i:ident : $rhs:path| -> $out:path $body:block) => (
+        impl_op_ex!($op |$lhs_i : &$lhs, $rhs_i : $rhs| -> $out $body);
+
+        _parse_binary_op!($op, $rhs, &$lhs, $out, lhs, rhs, {|$lhs_i : &$lhs, $rhs_i : $rhs| -> $out {$body} (rhs, lhs)});
+        _parse_binary_op!($op, $rhs, $lhs, $out, lhs, rhs, {|$lhs_i : &$lhs, $rhs_i : $rhs| -> $out {$body} (&rhs, lhs)});
+    );
+    ($op:tt |$lhs_i:ident : $lhs:path, $rhs_i:ident : &$rhs:path| -> $out:path $body:block) => (
+        impl_op_ex!($op |$lhs_i : $lhs, $rhs_i : &$rhs| -> $out $body);
+
+        _parse_binary_op!($op, &$rhs, $lhs, $out, lhs, rhs, {|$lhs_i : $lhs, $rhs_i : &$rhs| -> $out {$body} (rhs, lhs)});
+        _parse_binary_op!($op, $rhs, $lhs, $out, lhs, rhs, {|$lhs_i : $lhs, $rhs_i : &$rhs| -> $out {$body} (rhs, &lhs)});
+    );
+    ($op:tt |$lhs_i:ident : $lhs:path, $rhs_i:ident : $rhs:path| -> $out:path $body:block) => (
+        impl_op_commutative!($op |$lhs_i : $lhs, $rhs_i : $rhs| -> $out $body);
+    );
+}
 
 #[macro_export]
 macro_rules! _parse_binary_op {
@@ -58,47 +147,17 @@ macro_rules! _parse_unary_op {
 
 #[macro_export]
 macro_rules! _impl_binary_op_internal {
-    ($ops_trait:ident, $ops_fn:ident, &$lhs:ty, &$rhs:ty, $out:ty, $fn:expr) => (
-        _impl_binary_op_owned_owned!($ops_trait, $ops_fn, $lhs, $rhs, $out, lhs, rhs, {$fn(&lhs, &rhs)});
-        _impl_binary_op_owned_borrowed!($ops_trait, $ops_fn, $lhs, $rhs, $out, lhs, rhs, {$fn(&lhs, rhs)});
-        _impl_binary_op_borrowed_owned!($ops_trait, $ops_fn, $lhs, $rhs, $out, lhs, rhs, {$fn(lhs, &rhs)});
-        _impl_binary_op_borrowed_borrowed!($ops_trait, $ops_fn, $lhs, $rhs, $out, lhs, rhs, {$fn(lhs, rhs)});
+    ($ops_trait:ident, $ops_fn:ident, &$lhs:ty, &$rhs:ty, $out:ty, $lhs_i:ident, $rhs_i:ident, $body:block) => (
+        _impl_binary_op_borrowed_borrowed!($ops_trait, $ops_fn, $lhs, $rhs, $out, $lhs_i, $rhs_i, $body);
     );
-    ($ops_trait:ident, $ops_fn:ident, &$lhs:ty, $rhs:ty, $out:ty, $fn:expr) => (
-        _impl_binary_op_owned_owned!($ops_trait, $ops_fn, $lhs, $rhs, $out, lhs, rhs, {$fn(&lhs, rhs)});
-        _impl_binary_op_borrowed_owned!($ops_trait, $ops_fn, $lhs, $rhs, $out, lhs, rhs, {$fn(lhs, rhs)});
+    ($ops_trait:ident, $ops_fn:ident, &$lhs:ty, $rhs:ty, $out:ty, $lhs_i:ident, $rhs_i:ident, $body:block) => (
+        _impl_binary_op_borrowed_owned!($ops_trait, $ops_fn, $lhs, $rhs, $out, $lhs_i, $rhs_i, $body);
     );
-    ($ops_trait:ident, $ops_fn:ident, $lhs:ty, &$rhs:ty, $out:ty, $fn:expr) => (
-        _impl_binary_op_owned_owned!($ops_trait, $ops_fn, $lhs, $rhs, $out, lhs, rhs, {$fn(lhs, &rhs)});
-        _impl_binary_op_owned_borrowed!($ops_trait, $ops_fn, $lhs, $rhs, $out, lhs, rhs, {$fn(lhs, rhs)});
+    ($ops_trait:ident, $ops_fn:ident, $lhs:ty, &$rhs:ty, $out:ty, $lhs_i:ident, $rhs_i:ident, $body:block) => (
+        _impl_binary_op_owned_borrowed!($ops_trait, $ops_fn, $lhs, $rhs, $out, $lhs_i, $rhs_i, $body);
     );
-    ($ops_trait:ident, $ops_fn:ident, $lhs:ty, $rhs:ty, $out:ty, $fn:expr) => (
-        _impl_binary_op_owned_owned!($ops_trait, $ops_fn, $lhs, $rhs, $out, lhs, rhs, {$fn(lhs, rhs)});
-    );
-    ($ops_trait:ident, $ops_fn:ident, [commutative], &$lhs:ty, &$rhs:ty, $out:ty, $fn:expr) => (
-        _impl_binary_op_internal!($ops_trait, $ops_fn, &$lhs, &$rhs, $out, $fn);
-
-        _impl_binary_op_owned_owned!($ops_trait, $ops_fn, $rhs, $lhs, $out, lhs, rhs, {$fn(&rhs, &lhs)});
-        _impl_binary_op_owned_borrowed!($ops_trait, $ops_fn, $rhs, $lhs, $out, lhs, rhs, {$fn(rhs, &lhs)});
-        _impl_binary_op_borrowed_owned!($ops_trait, $ops_fn, $rhs, $lhs, $out, lhs, rhs, {$fn(&rhs, lhs)});
-        _impl_binary_op_borrowed_borrowed!($ops_trait, $ops_fn, $rhs, $lhs, $out, lhs, rhs, {$fn(rhs, lhs)});
-    );
-    ($ops_trait:ident, $ops_fn:ident, [commutative], &$lhs:ty, $rhs:ty, $out:ty, $fn:expr) => (
-        _impl_binary_op_internal!($ops_trait, $ops_fn, &$lhs, $rhs, $out, $fn);
-
-        _impl_binary_op_owned_owned!($ops_trait, $ops_fn, $rhs, $lhs, $out, lhs, rhs, {$fn(&rhs, lhs)});
-        _impl_binary_op_owned_borrowed!($ops_trait, $ops_fn, $rhs, $lhs, $out, lhs, rhs, {$fn(rhs, lhs)});
-    );
-    ($ops_trait:ident, $ops_fn:ident, [commutative], $lhs:ty, &$rhs:ty, $out:ty, $fn:expr) => (
-        _impl_binary_op_internal!($ops_trait, $ops_fn, $lhs, &$rhs, $out, $fn);
-
-        _impl_binary_op_owned_owned!($ops_trait, $ops_fn, $rhs, $lhs, $out, lhs, rhs, {$fn(rhs, &lhs)});
-        _impl_binary_op_borrowed_owned!($ops_trait, $ops_fn, $rhs, $lhs, $out, lhs, rhs, {$fn(rhs, lhs)});
-    );
-    ($ops_trait:ident, $ops_fn:ident, [commutative], $lhs:ty, $rhs:ty, $out:ty, $fn:expr) => (
-        _impl_binary_op_internal!($ops_trait, $ops_fn, $lhs, $rhs, $out, $fn);
-
-        _impl_binary_op_owned_owned!($ops_trait, $ops_fn, $rhs, $lhs, $out, lhs, rhs, {$fn(rhs, lhs)});
+    ($ops_trait:ident, $ops_fn:ident, $lhs:ty, $rhs:ty, $out:ty, $lhs_i:ident, $rhs_i:ident, $body:block) => (
+        _impl_binary_op_owned_owned!($ops_trait, $ops_fn, $lhs, $rhs, $out, $lhs_i, $rhs_i, $body);
     );
 }
 
@@ -164,23 +223,19 @@ macro_rules! _impl_binary_op_borrowed_borrowed {
 
 #[macro_export]
 macro_rules! _impl_assignment_op_internal {
-    ($ops_trait:ident, $ops_fn:ident, $lhs:ty, &$rhs:ty, $fn:expr) => (        
-        impl ops::$ops_trait<$rhs> for $lhs {
-            fn $ops_fn(&mut self, rhs: $rhs) {
-                $fn(self, &rhs)
-            }
-        }
-
+    ($ops_trait:ident, $ops_fn:ident, $lhs:ty, &$rhs:ty, $lhs_i:ident, $rhs_i:ident, $body:block) => (        
         impl<'a> ops::$ops_trait<&'a $rhs> for $lhs {
-            fn $ops_fn(&mut self, rhs: &$rhs) {
-                $fn(self, rhs)
+            fn $ops_fn(&mut self, $rhs_i: &$rhs) {
+                let mut $lhs_i = self;
+                $body
             }
         }
     );
-    ($ops_trait:ident, $ops_fn:ident, $lhs:ty, $rhs:ty, $fn:expr) => (        
+    ($ops_trait:ident, $ops_fn:ident, $lhs:ty, $rhs:ty, $lhs_i:ident, $rhs_i:ident, $body:block) => (        
         impl ops::$ops_trait<$rhs> for $lhs {
-            fn $ops_fn(&mut self, rhs: $rhs) {
-                $fn(self, rhs)
+            fn $ops_fn(&mut self, $rhs_i: $rhs) {
+                let mut $lhs_i = self;
+                $body
             }
         }
     );
@@ -188,29 +243,23 @@ macro_rules! _impl_assignment_op_internal {
 
 #[macro_export]
 macro_rules! _impl_unary_op_internal {
-    ($ops_trait:ident, $ops_fn:ident, &$lhs:ty, $out:ty, $fn:expr) => (        
-        impl ops::$ops_trait for $lhs {
-            type Output = $out;
-
-            fn $ops_fn(self) -> Self::Output {
-                $fn(&self)
-            }
-        }
-
+    ($ops_trait:ident, $ops_fn:ident, &$lhs:ty, $out:ty, $lhs_i:ident, $body:block) => (        
         impl<'a> ops::$ops_trait for &'a $lhs {
             type Output = $out;
 
             fn $ops_fn(self) -> Self::Output {
-                $fn(self)
+                let $lhs_i = self;
+                $body
             }
         }
     );
-    ($ops_trait:ident, $ops_fn:ident, $lhs:ty, $out:ty, $fn:expr) => (        
+    ($ops_trait:ident, $ops_fn:ident, $lhs:ty, $out:ty, $lhs_i:ident, $body:block) => (        
         impl ops::$ops_trait for $lhs {
             type Output = $out;
 
             fn $ops_fn(self) -> Self::Output {
-                $fn(self)
+                let $lhs_i = self;
+                $body
             }
         }
     );
