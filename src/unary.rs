6ba9dfcab549acf27d36cf05aa3c1f7268c28e1a
Brian Porter <brianwp3000@gmail.com>
2017-06-28 09:14:29 -0700

    separated internal macros to their own mods

---
+++ src/unary.rs
@@ -0,0 +1,23 @@
+#[macro_export]
+macro_rules! _impl_unary_op_internal {
+    ($ops_trait:ident, $ops_fn:ident, &$lhs:ty, $out:ty, $lhs_i:ident, $body:block) => (        
+        impl<'a> ops::$ops_trait for &'a $lhs {
+            type Output = $out;
+
+            fn $ops_fn(self) -> Self::Output {
+                let $lhs_i = self;
+                $body
+            }
+        }
+    );
+    ($ops_trait:ident, $ops_fn:ident, $lhs:ty, $out:ty, $lhs_i:ident, $body:block) => (        
+        impl ops::$ops_trait for $lhs {
+            type Output = $out;
+
+            fn $ops_fn(self) -> Self::Output {
+                let $lhs_i = self;
+                $body
+            }
+        }
+    );
+}
